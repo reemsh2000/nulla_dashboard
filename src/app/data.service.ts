b05253f66103349a9788a5f1ec5453b55d5e18b3
reemsh2000 <sh951652@gmail.com>
2022-05-11 10:06:41 +0300

    create data service for get questions snd response of input survey

---
+++ src/app/data.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  apiUrl = 'https://api.typeform.com/forms/';
+  headers = new HttpHeaders({
+    "Authorization": "Bearer tfp_2Ph2w1NhEaFUmqoLFnsDGQo72i7VHt3uvjYXtvtow9hv_3mJs7NpZAGDsrf",
+  });
+
+  constructor(private http: HttpClient) { }
+  getQuestions(formId: string) {
+    return this.http.get(`${this.apiUrl}${formId}`, { headers: this.headers })
+
+  }
+  getAnswers(formId: string) {
+    return this.http.get(`${this.apiUrl}${formId}/responses`, { headers: this.headers })
+  }
+
+}
