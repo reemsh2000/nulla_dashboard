505a1e5da90a90f9642ac0fa669ad03609f74f59
reemsh2000 <sh951652@gmail.com>
2022-05-17 12:07:32 +0300

    stack service

--- src/app/components/stack-charts/stack-charts.component.ts
+++ src/app/components/stack-charts/stack-charts.component.ts
@@ -1,21 +1,34 @@
 import { Statistics } from './../../Interfaces/interfaces';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { StackChartsService } from './stack-charts.service';
 @Component({
   selector: 'app-stack-charts',
   templateUrl: './stack-charts.component.html',
   styleUrls: ['./stack-charts.component.css']
 })
-export class StackChartsComponent implements OnInit {
-  PersonalityStatistics: Statistics;
-
-  constructor(private stackService: StackChartsService) {
-    this.PersonalityStatistics = this.stackService.getPersonalityAnswersStatistics('TNatOmnO');
-
+export class StackChartsComponent implements OnInit, OnDestroy {
+  personalityStatistics: Statistics = {
+    agree: 0,
+    neutral: 0,
+    disagree: 0
+  }
+  leadStatistics: Statistics = {
+    agree: 0,
+    neutral: 0,
+    disagree: 0
   }
 
+  constructor(public stackService: StackChartsService) {
+    this.personalityStatistics = this.stackService.getQuestionStatistics('UzkZtaLj', this.stackService.pesrsonalityQuestions);
+    // this.leadStatistics = this.stackService.getQuestionStatistics('UzkZtaLj', this.stackService.leadQuestions);
+    console.log("personalityStatistics", this.stackService.getPersonaResult);
+  }
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   }
 
-}
--- src/app/components/stack-charts/stack-charts.service.ts
+++ src/app/components/stack-charts/stack-charts.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { BehaviorSubject, mergeMap, tap } from 'rxjs';
+import { Injectable, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
 import { Question, Statistics } from '../../Interfaces/interfaces';
 
@@ -6,59 +7,106 @@ import { Question, Statistics } from '../../Interfaces/interfaces';
   providedIn: 'root'
 })
 export class StackChartsService {
-  personalityQuestions: Question[] = [];
-  personalityQuestionsIndex = 4;
+  private personalityStatistics = new BehaviorSubject<Statistics>({
+    agree: 0,
+    neutral: 0,
+    disagree: 0
+
+  })
+  public personalityStatistics$ = this.personalityStatistics.asObservable();
+  get getPersonaResult(): Statistics {
+    return this.personalityStatistics.getValue();
+  }
+
+  private set getPersonalResult(val: Statistics) {
+    this.personalityStatistics.next(val);
+  }
+  pesrsonalityQuestions: Question[] = []
+  leadQuestions: Question[] = [];
+
 
   constructor(private dataService: DataService) {
-    this.personalityQuestions = this.getPersonalityQuestions("TNatOmnO");
+    this.getFirstQuestionRef("UzkZtaLj", "personal", (questionRef: string) => {
+      this.getQuestions("UzkZtaLj", questionRef, (value: any) => {
+        this.pesrsonalityQuestions.push(value)
+      })
+    });
+    this.getFirstQuestionRef("UzkZtaLj", "lead_question", (questionRef: string) => {
+      this.getQuestions("UzkZtaLj", questionRef, (value: any) => {
+        this.leadQuestions.push(value)
+      })
+    }
+    );
+  }
+
+  getFirstQuestionRef(surveyId: string, questionType: string, cb: any): void {
+    this.dataService.getQuestions(surveyId).subscribe((res: any) => {
+      for (let i = 0; i < res.logic.length; i++) {
+        if (res.logic[i].actions[0].details.value.value === questionType) {
+          cb(res.logic[i].ref);
+          break;
+        }
+      }
+    })
   }
 
   //  Get the personality questions for the stack chart
-  getPersonalityQuestions(surveyId: string): Question[] {
+  getQuestions(surveyId: string, firstRef: string, cb: any): void {
     let personalityQuestion: Question[] = []
     this.dataService.getQuestions(surveyId).subscribe((res: any) => {
-      for (let i = 0; i <= 5; i++) {
-        personalityQuestion.push(res.fields[this.personalityQuestionsIndex].properties.fields[i]);
-      }
+      res.fields.forEach((questions: any) => {
+        for (let i = 0; i < questions.properties.fields?.length; i++) {
+          if (questions.properties.fields[i].ref === firstRef) {
+            questions.properties.fields.forEach((question: any) => {
+              cb(question);
+            });
+          }
+
+        }
+        return;
+      })
     });
-    return personalityQuestion;
+
   }
 
+
   //  Get the statistics of perosnality Questions for the stack chart
-  getPersonalityAnswersStatistics(surveyId: string): Statistics {
-    let personalityQusetionsStatistics: Statistics = {
+  getQuestionStatistics(surveyId: string, questions: Question[]) {
+    let statistics: Statistics = {
       agree: 0,
       neutral: 0,
       disagree: 0,
     }
+
     this.dataService.getAnswers(surveyId).subscribe((answers: any) => {
       answers.items.forEach((surveyResponse: any) => {
         surveyResponse.answers.forEach((answer: any) => {
-          this.personalityQuestions.forEach((peronalityQusetion: any) => {
-            if (answer.field.id === peronalityQusetion.id) {
+          questions.forEach((question: any) => {
+            if (answer.field.id === question.id) {
               switch (answer.choice.label) {
                 case 'Strongly agree':
                 case 'Agree':
-                  personalityQusetionsStatistics['agree'] = personalityQusetionsStatistics['agree'] + 1;
+                  statistics['agree'] = statistics['agree'] + 1;
                   break;
                 case 'Disagree':
                 case 'Strongly disagree':
-                  personalityQusetionsStatistics['disagree'] = personalityQusetionsStatistics['disagree'] + 1;
+                  statistics['disagree'] = statistics['disagree'] + 1;
                   break;
                 case 'Neutral':
-                  personalityQusetionsStatistics['neutral'] = personalityQusetionsStatistics['neutral'] + 1;
+                  statistics['neutral'] = statistics['neutral'] + 1;
                   break;
               }
             }
 
-          }); //questions loop end 
+
+          });
+
+          //questions loop end 
         }) //answers loop end 
       });// survey response loop end 
+      this.getPersonalResult = statistics;
     }
-
-    ); // request end 
-    return personalityQusetionsStatistics;
+    );
+    return statistics;
   }
 }
-
-
