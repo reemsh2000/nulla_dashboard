d7b4d35144c46bf54ca535ddb3317fad8d4efbcc
reemsh2000 <sh951652@gmail.com>
2022-05-26 15:28:19 +0300

    create cach intercptor

---
+++ src/app/cache-interceptor.interceptor.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpResponse
+} from '@angular/common/http';
+import { Observable, of, tap } from 'rxjs';
+
+@Injectable()
+export class CacheInterceptorInterceptor implements HttpInterceptor {
+
+  private cache: Map<HttpRequest<any>, HttpResponse<any>> = new Map();
+
+  constructor() {}
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {    
+    if(req.method !== "GET") {
+      return next.handle(req);
+    }
+    const cachedResponse: HttpResponse<any>|undefined = this.cache.get(req)
+    // console.log(cachedResponse);
+    if(cachedResponse) {
+      // console.log('cache hit');
+      
+        return of(cachedResponse.clone())
+    }else {
+      // console.log('cache miss');
+        return next.handle(req).pipe(tap( (stateEvent:any) => {
+          // console.log('object :>> ', stateEvent);
+                if(stateEvent instanceof HttpResponse) {
+                    this.cache.set(req, stateEvent.clone())
+                    // console.log('cache set');
+                }
+            }
+        ));
+    }
+  }
+
+
+  
+  
+}
