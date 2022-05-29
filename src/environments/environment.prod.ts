3399ebb595e60caf12cb448fbf0b90bdc339e88a
reemsh2000 <sh951652@gmail.com>
2022-05-29 10:39:02 +0300

    add production files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA9QInKlIEyN9tzgB58suvB1o4BF_BP5DE',
+    authDomain: 'auth-new-ed87a.firebaseapp.com',
+    projectId: 'auth-new-ed87a',
+    storageBucket: 'auth-new-ed87a.appspot.com',
+    messagingSenderId: '154357492020',
+    appId: '1:154357492020:web:c6bc6b104a92209b635d61',
+    measurementId: 'G-MDSHPFVFFJ',
+  },
 };
