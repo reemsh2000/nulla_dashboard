a2769472a365af4dd384eb6dd979a997da23ec6e
reemsh2000 <sh951652@gmail.com>
2022-06-02 14:23:24 +0300

    firebase intialization

---
+++ functions/src/firebase.js
@@ -0,0 +1,12 @@
+import * as admin from "firebase-admin";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDaOdXLP0G-3CK70oMKrxuHHrhIMIG4LsI",
+  authDomain: "nulla-316b1.firebaseapp.com",
+  projectId: "nulla-316b1",
+  storageBucket: "nulla-316b1.appspot.com",
+  messagingSenderId: "687533639630",
+  appId: "1:687533639630:web:a0e61408bfe513af5dcd9f",
+};
+
+admin.initializeApp(firebaseConfig);
