8331f95bedb47e34fb4e39f31f2addf5ba6e6e50
rawand1998 <rawandgaradh@gmail.com>
2022-06-05 11:48:47 +0300

    images in admin page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './Router/app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthImageSideComponent } from './components/common/auth-image-side/auth-image-side.component';
-
+import { LoginComponent } from './components/Auth/login/login.component';
+import { SignupComponent } from './components/Auth/signup/signup.component';
+// import { ImportSurveyComponent } from './components/Dashborad/import-survey/import-survey.component';
+// import { SurveysComponent } from './components/Dashborad/surveys/surveys.component';
+// import { RecommendationsComponent } from './components/Dashborad/recommendations/recommendations.component';
 import { CompanyAccountComponent } from './components/company/company-account/company-account.component';
 import { InterestsComponent } from './components/company/interests/interests.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { HttpClientModule } from '@angular/common/http';
+import {ProgressSpinnerModule} from 'primeng/progressspinner';
 import { environment } from '../environments/environment';
 
 import { StackChartComponent } from './components/common/stack-chart/stack-chart.component';
@@ -18,23 +22,31 @@ import { SnapshotComponent } from './components/snapshot/snapshot.component';
 import { SearchComponent } from './components/search/search.component';
 import { AgeStatisticsComponent } from './components/age-statistics/age-statistics.component';
 import { FormsModule } from '@angular/forms';
-import { routingTable } from './routes'
-import { ChartModule } from 'primeng/chart';
+import {routingTable} from './routes'
+import {ChartModule} from 'primeng/chart';
 import { NavComponent } from './components/common/nav/nav.component';
 import { LocationComponent } from './components/location/location.component';
 import { TeamsComponent } from './components/teams/teams.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WelcomepageComponent } from './components/welcomepage/welcomepage.component';
 import { DasboradComponent } from './components/dasborad/dasborad.component';
-import { RouterModule } from '@angular/router';
-import { AngularFireModule } from '@angular/fire/compat'
+import {RouterModule} from '@angular/router';
+import {AngularFireModule} from '@angular/fire/compat'
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { CacheInterceptorInterceptor } from './cache-interceptor.interceptor';
+import {AngularFirestoreModule} from '@angular/fire/compat/firestore';
+import { ResetpasswordComponent } from './components/resetpassword/resetpassword.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from './components/profile/profile.component';
+import { MatTableModule } from '@angular/material/table';
+import { AdminpageComponent } from './components/adminpage/adminpage.component'  
 @NgModule({
   declarations: [
     AppComponent,
     AuthImageSideComponent,
+    LoginComponent,
+    SignupComponent,
+    // SurveysComponent,
+    // RecommendationsComponent,
     CompanyAccountComponent,
     InterestsComponent,
     StackChartComponent,
@@ -50,8 +62,13 @@ import { CacheInterceptorInterceptor } from './cache-interceptor.interceptor';
     TeamsComponent,
     WelcomepageComponent,
     DasboradComponent,
+    ResetpasswordComponent,
+    ProfileComponent,
+    AdminpageComponent,
   ],
   imports: [
+    MatTableModule,
+    ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     RouterModule.forRoot(routingTable),
@@ -64,19 +81,13 @@ import { CacheInterceptorInterceptor } from './cache-interceptor.interceptor';
     ProgressSpinnerModule,
     FormsModule,
     BrowserAnimationsModule,
-
-
+   
+    
     // FontAwesomeModule,
     // FaIconLibrary, initialize
 
   ],
-  providers: [
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: CacheInterceptorInterceptor,
-      multi: true
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,42 +2,148 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Router } from '@angular/router';
-
+import { GoogleAuthProvider, signInWithPopup } from '@angular/fire/auth';
+import { BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor(private auth: AngularFireAuth, private router: Router ,public firestore: AngularFirestore ) {
-    
+  loginValue: boolean;
+  completeform: boolean;
+  adminInfo: any;
+  item: any;
+  constructor(
+    private auth: AngularFireAuth,
+    private router: Router,
+    public firestore: AngularFirestore
+  ) {}
+  // private userId = new BehaviorSubject<string>('');
+  // public userId$ = this.userId.asObservable();
+
+  userId = '';
+
+  checkAuth() {
+    this.auth.onAuthStateChanged((user: any) => {
+      if (user) {
+        console.log(user.uid);
+        this.userId = user.uid;
+      } else {
+        console.log('no authentication');
+      }
+    });
   }
-  
-  register(email: string, password: string ,name: string,phone:number) {
-    this.auth['createUserWithEmailAndPassword'](email, password).then(
+
+  register(form: any) {
+    this.auth['createUserWithEmailAndPassword'](form.email, form.password).then(
       (res: { user: any }) => {
-    
-     this.firestore.collection('admin').doc(res.user.uid).set({email,name,phone}).then(()=>{
-      this.router.navigate(['/login'])
-     })
-  
+        this.userId = res.user.uid;
+        this.firestore
+          .collection('admin')
+          .doc(res.user.uid)
+          .set({})
+          .then(() => {
+            this.firestore
+              .collection('profile-company')
+              .doc(res.user.uid)
+              .set({})
+              .then(() => {
+                console.log('add last');
+              })
+              .then(() => {
+                this.firestore
+                  .collection('profile')
+                  .doc(res.user.uid)
+                  .set({})
+                  .then(() => {
+                    console.log('add last');
+                  });
+              });
+          })
+          .then(() => {
+            this.router.navigate(['/welcome']);
+          });
       }
     );
-
   }
 
-  admin(Record:any){
-    this.firestore.collection('admin').add(Record).then(()=>{
-     
-    }).catch(err=>{
-      console.error(err)
-    })
+  admin(Record: any) {
+    this.firestore
+      .collection('admin')
+      .doc(this.userId)
+      .update(Record)
+      .then(() => {})
+      .catch((err) => {
+        console.error(err);
+      });
   }
-  login(email:string,password:string){
-    this.auth['signInWithEmailAndPassword'](email,password).then((res: { user: any; })=>{
-      localStorage.setItem('token','true')
-      if(res.user){
-        this.router.navigate(['/dashborad']);
+
+  profileCompany(Record: any) {
+    this.firestore
+      .collection('profile-company')
+      .doc(this.userId)
+      .update(Record)
+      .then(() => {
+        this.completeform = true;
+        this.router.navigate(['/intrest']);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  intrestQuestions(Record: any) {
+    this.firestore
+      .collection('intersetQuestion')
+      .doc(this.userId)
+      .set(Record)
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  login(form: any) {
+    this.auth['signInWithEmailAndPassword'](form.email, form.password).then(
+      (res: { user: any }) => {
+        localStorage.setItem('token', this.userId);
+        this.loginValue = true;
+        if (res.user) {
+          this.router.navigate(['/dashborad']);
+        }
       }
-    })
+    );
   }
-
+  ResetPassword(email: string) {
+    this.auth.sendPasswordResetEmail(email).then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
+  logout() {
+    this.auth.signOut().then(() => {
+      this.router.navigate(['/login']);
+      this.loginValue = false;
+    });
+  }
+  loginWithGoogle() {
+    this.auth.signInWithPopup(new GoogleAuthProvider()).then(() => {
+      this.router.navigate(['/dashborad']);
+    });
+  }
+  profile(Record: any) {
+    this.firestore
+      .collection('profile')
+      .doc(this.userId)
+      .set(Record)
+      .then(() => {
+        this.completeform = true;
+        this.router.navigate(['/adminpage']);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+  getProfileData() {
+    return this.firestore.collection('profile').doc(this.userId).get();
+  }
 }
