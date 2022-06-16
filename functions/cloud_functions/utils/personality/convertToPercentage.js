15c01e9eebd611724f7ddd90dc27b61e81ea7d5d
reemsh2000 <sh951652@gmail.com>
2022-06-16 13:54:49 +0300

    add inital value for title params

--- functions/cloud_functions/utils/personality/convertToPercentage.js
+++ functions/cloud_functions/utils/personality/convertToPercentage.js
@@ -1,4 +1,4 @@
-const convertToPercentage = ({ agree, neutral, disagree, title }) => {
+const convertToPercentage = ({ agree, neutral, disagree, title='' }) => {
   const total = agree + neutral + disagree;
   const agreePer = Math.round((agree / total) * 100);
   const neutralPer = Math.round((neutral / total) * 100);
